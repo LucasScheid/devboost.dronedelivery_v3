4f033714cb5b0c386ac8541095c6a32d2da4388c
Felipe Miranda <lipemiranda159@hotmail.com>
2020-09-02 18:28:41 -0300

    add cliente extensions tests

---
+++ devboost.dronedelivery.test/DTO/Extensions/ClienteExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using devboost.dronedelivery.felipe.DTO.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace devboost.dronedelivery.test.DTO.Extensions
+{
+    public class ClienteExtensionsTests
+    {
+        [Fact]
+        public void ClienteHasUserTest()
+        {
+            var cliente = SetupTests.GetCliente();
+            Assert.True(cliente.HasClient());
+        }
+    }
+}
