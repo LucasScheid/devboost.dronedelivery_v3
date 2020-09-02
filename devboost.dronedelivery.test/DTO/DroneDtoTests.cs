9e168aae18984ae554684d5ff04f80c52a9f2e17
lipemiranda159 <lipemiranda159@hotmail.com>
2020-09-02 16:24:06 -0300

    Add dto tests

---
+++ devboost.dronedelivery.test/DTO/DroneDtoTests.cs
@@ -0,0 +1,26 @@
+﻿using devboost.dronedelivery.felipe.DTO;
+using Xunit;
+
+namespace devboost.dronedelivery.test.DTO
+{
+    public class DroneDtoTests
+    {
+        [Fact]
+        public void TestConstructor()
+        {
+            var droneDto = new DroneDto(
+                    new DroneStatusDto()
+                    {
+                        Drone = SetupTests.GetDrone(),
+                        SomaDistancia = 100,
+                        SomaPeso = 100
+                    }, 100);
+
+            Assert.True(droneDto.Distancia == 100);
+            Assert.True(droneDto.DroneStatus.SomaDistancia == 100);
+            Assert.True(droneDto.DroneStatus.SomaPeso == 100);
+
+
+        }
+    }
+}
