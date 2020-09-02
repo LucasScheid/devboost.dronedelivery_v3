f6cce4738d19487aac8f4c455c2e0e865ee1eb67
Lucas Scheid <lucas.scheid@penske.com>
2020-09-01 22:00:59 -0300

    Teste Criação de Pedido e Função status Drone;

---
+++ devboost.dronedelivery.test/Drone/ConsultaStatusDroneUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using devboost.dronedelivery.felipe.EF.Repositories.Interfaces;
+using devboost.dronedelivery.felipe.Services;
+using devboost.dronedelivery.felipe.Services.Interfaces;
+using Xunit;
+
+namespace devboost.dronedelivery.test.Drone
+{
+    public class ConsultaStatusDroneUnitTest
+    {
+        [Fact]
+        public void ConsultarStatusDrone()
+        {
+            ICoordinateService coordinateService = new CoordinateService();
+            IPedidoDroneRepository pedidoDroneRepository = null;
+            IDroneRepository droneRepository = new MockDroneRepository();
+            IPedidoRepository pedidoRepository = null;
+
+            IDroneService droneService = new DroneService(coordinateService, pedidoDroneRepository, droneRepository, pedidoRepository);
+
+            droneService.GetDroneStatusAsync();
+
+            Assert.Equal<int>(2,droneService.GetDroneStatusAsync().Count);
+        }
+    }
+}
