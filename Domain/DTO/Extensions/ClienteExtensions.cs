52db495340656137755c35a08f905328c1e4b75b
lipemiranda159 <lipemiranda159@hotmail.com>
2020-09-02 16:25:12 -0300

    Add cliente extensions

---
+++ Domain/DTO/Extensions/ClienteExtensions.cs
@@ -0,0 +1,12 @@
+﻿using devboost.dronedelivery.felipe.DTO.Models;
+
+namespace devboost.dronedelivery.felipe.DTO.Extensions
+{
+    public static class ClienteExtensions
+    {
+        public static bool IsUserEmpty(this Cliente cliente)
+        {
+            return cliente != null && string.IsNullOrEmpty(cliente.UserId);
+        }
+    }
+}
