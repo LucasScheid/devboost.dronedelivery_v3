9c3e5a7443d0eb2f5fdbb6c28cac99f7ef7b9750
lipemiranda159 <lipemiranda159@hotmail.com>
2020-09-02 16:32:14 -0300

    Create loginValidator

---
+++ Services/Security/LoginValidator.cs
@@ -0,0 +1,22 @@
+﻿using devboost.dronedelivery.felipe.DTO.Models;
+using devboost.dronedelivery.felipe.Security.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace devboost.dronedelivery.felipe.Security
+{
+    public class LoginValidator : ILoginValidator
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public LoginValidator(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> CheckUserAndPassword(ApplicationUser user, string password, bool lockoutOnFailure)
+        {
+            var result = await _signInManager.CheckPasswordSignInAsync(user, password, lockoutOnFailure);
+            return result.Succeeded;
+        }
+    }
+}
