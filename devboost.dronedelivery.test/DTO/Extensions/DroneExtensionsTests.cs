478e535eeea7e759ee86f40a3692c4933eb44157
Felipe Miranda <lipemiranda159@hotmail.com>
2020-09-02 18:34:34 -0300

    add drone extensions test

---
+++ devboost.dronedelivery.test/DTO/Extensions/DroneExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using devboost.dronedelivery.felipe.DTO.Extensions;
+
+namespace devboost.dronedelivery.test.DTO.Extensions
+{
+    public class DroneExtensionsTests
+    {
+        [Fact]
+        public void TestToTempoGasto()
+        {
+            var drone = SetupTests.GetDrone();
+            var date = drone.ToTempoGasto(100);
+            Assert.True(date.Hour == DateTime.Now.AddHours(2).Hour);
+        }
+    }
+}
