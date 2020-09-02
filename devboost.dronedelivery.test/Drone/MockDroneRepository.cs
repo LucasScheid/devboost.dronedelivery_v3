b3d971776b92bc2f453867eac53a7ab336d67bd2
lipemiranda159 <lipemiranda159@hotmail.com>
2020-09-02 16:23:31 -0300

    adapt for tests

--- devboost.dronedelivery.test/Drone/MockDroneRepository.cs
+++ devboost.dronedelivery.test/Drone/MockDroneRepository.cs
@@ -1,5 +1,4 @@
 ﻿using devboost.dronedelivery.felipe.DTO;
-using devboost.dronedelivery.felipe.DTO.Enums;
 using devboost.dronedelivery.felipe.EF.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -15,35 +14,7 @@ namespace devboost.dronedelivery.test.Drone
 
         public List<StatusDroneDto> GetDroneStatusAsync()
         {
-            List<StatusDroneDto> statusDroneDtos = new List<StatusDroneDto>();
-
-            StatusDroneDto statusDroneDto = new StatusDroneDto
-            {
-                ClienteId = 1,
-                DroneId = 1,
-                Latitude = -23.5880684,
-                Longitude = -46.6564195,
-                Situacao = true,
-                PedidoId = 0,
-                Nome = string.Empty
-            };
-
-            statusDroneDtos.Add(statusDroneDto);
-
-            StatusDroneDto statusDroneDto2 = new StatusDroneDto
-            {
-                ClienteId = 1,
-                DroneId = 1,
-                Latitude = -23.5880684,
-                Longitude = -46.6564195,
-                Situacao = false,
-                PedidoId = 1,
-                Nome = "João da Silva"
-            };
-
-            statusDroneDtos.Add(statusDroneDto2);
-
-            return statusDroneDtos;
+            return SetupTests.GetDroneStatus();
         }
 
         public felipe.DTO.Models.Drone RetornaDrone()
