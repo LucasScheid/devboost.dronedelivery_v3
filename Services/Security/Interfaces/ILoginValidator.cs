47e4ecf0bb1ea4a86b146a731060862accdddfe7
lipemiranda159 <lipemiranda159@hotmail.com>
2020-09-02 16:31:31 -0300

    Create ILoginValidator

---
+++ Services/Security/Interfaces/ILoginValidator.cs
@@ -0,0 +1,10 @@
+﻿using devboost.dronedelivery.felipe.DTO.Models;
+using System.Threading.Tasks;
+
+namespace devboost.dronedelivery.felipe.Security.Interfaces
+{
+    public interface ILoginValidator
+    {
+        Task<bool> CheckUserAndPassword(ApplicationUser user, string password, bool lockoutOnFailure);
+    }
+}
